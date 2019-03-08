0dfb1e09018365dd20670dc9523cf0177ded4d43
JijaLoqie <43857227+JijaLoqie@users.noreply.github.com>
2019-03-08 19:31:45 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int n;
+	cin >> a >> n;
+	int b = a;
+	int res = 1;
+
+		while (n > 0) {
+			if (n % 2 == 1) {
+				res *= b;
+			}
+			b *= b;
+			n /= 2;
+		}
+		cout << res;
+		system("pause");
+}
