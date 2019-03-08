3078ead9e775d80042177340ef550446845efe4a
JijaLoqie <43857227+JijaLoqie@users.noreply.github.com>
2019-03-08 19:38:12 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 
-
 using namespace std;
-int main()
-{
-	
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> factors;
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			n /= i;
+			factors.push_back(i);
+		}
+	}
+	if (n != 1) {
+		factors.push_back(n);
+	}
+	for (int i = 0; i < factors.size() - 1; i++) {
+		cout << factors[i] << '*';
+	}
+	cout << factors.back() << endl;
 	system("pause");
 }
+
