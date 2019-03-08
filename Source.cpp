1942f86efbaf72d18e3771ac87d4b940aae0473e
JijaLoqie <43857227+JijaLoqie@users.noreply.github.com>
2019-03-08 19:59:40 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	int k;
+	int ll = 0; // Количество строк
+	int bb = 0; // Количество символов в строке
+	int m;
+	cin >> n >> m;
+	vector<vector<int>> v(n, vector<int>(m));
+	while (ll<n)
+	{
+		cout << ll * bb << " ";
+		bb++;
+		if (bb == m) {
+			ll++;
+			bb = 0;
+			cout << endl;
+		}
+	}
+	system("pause");
+}
+
