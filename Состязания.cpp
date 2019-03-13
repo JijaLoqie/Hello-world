07ae234f9fd7cbf36b52695f715025f029105bf3
JijaLoqie <43857227+JijaLoqie@users.noreply.github.com>
2019-03-13 21:37:00 +0300

    Update Состязания.cpp

--- Состязания.cpp
+++ Состязания.cpp
@@ -1,43 +1,38 @@
-﻿#include <iostream>
-#include <cmath>
-#include <string>
-#include <queue>
+#include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
-int main()
-{
-	int n;
-	int m;
-	cin >> n >> m;
-	vector<vector<int>> v(n, vector<int>(m));
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> v[i][j];
+
+struct node {
+	int max_score, total_score, id;
+
+	node(int t_max, int t_total, int t_id) : max_score(t_max), total_score(t_total), id(t_id) {}
+};
+
+bool cmp(node &a, node &b) {
+	if (a.max_score == b.max_score) {
+		if (a.total_score == b.total_score) {
+			return a.id < b.id;
 		}
+		return a.total_score > b.total_score;
 	}
-	vector<int> vse(n);
-	int max;
-	for (int i = 0; i < n; i++)
-	{
-		max = 0;
-		for (int j = 0; j < m; j++)
-		{
-			max += v[i][j];		
-		}
-		vse[i] = max;
-	}
-	int maxvse = -1;
-	int d = -1;
-	for (int i = 0; i < n; i++)
-	{
-		if (maxvse < vse[i])
-		{
-			maxvse = vse[i];
-			d = i;
-		}
-	}
-	cout << maxvse << endl << d;
-	system("pause");
+	return a.max_score > b.max_score;
 }
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<node> v;
+	for (int i = 0; i < n; i++) {
+		int t_max = 0, t_total = 0;
+		for (int j = 0; j < m; j++) {
+			int cur_score;
+			cin >> cur_score;
+			t_max = max(t_max, cur_score);
+			t_total += cur_score;
+		}
+		v.push_back(node(t_max, t_total, i));
+	}
+	sort(v.begin(), v.end(), cmp);
+	cout << v[0].id << endl;
